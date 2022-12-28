fe1d134db1b456d7f2f4631f4ea4285da85124b8
Luiz Calaça <luizcalaca@gmail.com>
2022-12-28 20:52:37 -0300

    feat: adding AuthProvider to the index.tsx like this available in entire app

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/Auth/AuthProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
 
