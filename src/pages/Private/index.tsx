121a2054d931a9aa506379d3d4fdf5532316b8c0
Luiz Calaça <luizcalaca@gmail.com>
2022-12-28 21:18:52 -0300

    feat: adding user name in private page

--- src/pages/Private/index.tsx
+++ src/pages/Private/index.tsx
@@ -1,5 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
 export const Private = () => {
+    const auth = useContext(AuthContext);
+
     return (
-        <div>Private</div>
+        <div>
+            <h2>Página Privada</h2>
+
+            Olá, {auth.user?.name}, tudo bem?
+        </div>
     );
 }
